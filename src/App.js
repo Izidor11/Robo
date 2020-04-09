c876261556f7f182a427baa08da38acf264ccb99
tycoon <tycoon@tycoon.local>
2020-04-09 14:32:16 +0200

    Scrollable component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
   constructor() {
@@ -14,9 +15,7 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => {
-        response.json();
-      })
+      .then((response) => response.json())
       .then((users) => {
         this.setState({ robots: users });
       });
@@ -39,7 +38,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />;
+          <Scroll>
+            <CardList robots={filteredRobots} />;
+          </Scroll>
         </div>
       );
     }
