b2c6076d626d6e1325f0c82415087922759ba43e
tycoon <tycoon@tycoon.local>
2020-04-10 08:12:32 +0200

    Updates

---
+++ precache-manifest.48d683293cce67eb72b2e4533c4b3034.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2492610fead669e1758456d968dff7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3abacb31bea4188bbea6",
+    "url": "/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "ec70b64a7c20285a8fef",
+    "url": "/static/css/main.3d0fa094.chunk.css"
+  },
+  {
+    "revision": "3abacb31bea4188bbea6",
+    "url": "/static/js/2.8644df47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8644df47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec70b64a7c20285a8fef",
+    "url": "/static/js/main.a923d430.chunk.js"
+  },
+  {
+    "revision": "b81c2d9ae706f7e12811",
+    "url": "/static/js/runtime-main.ced909f6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
